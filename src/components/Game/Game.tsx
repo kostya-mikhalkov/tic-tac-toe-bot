0843cd7426fd6548eabcc0c32d2f96c8854a8051
unknown <kostya.mkhalkov@bk.ru>
2024-11-16 13:36:25 +0300

    init: start project

---
+++ src/components/Game/Game.tsx
@@ -0,0 +1,13 @@
+import React, {FunctionComponent} from "react";
+
+import './Game.scss';
+
+const Game: FunctionComponent = (): JSX.Element => {
+    return (
+        <div className="game">
+            
+        </div>
+    )
+}
+
+export default Game;
